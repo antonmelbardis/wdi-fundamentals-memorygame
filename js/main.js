e575377741a8461f8974ba84044e0b316a8afd83
Anton Melbardis <melbardis@gmail.com>
2017-06-02 15:03:53 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
+var cards = ["Queen", "Queen", "King", "King"]
 
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("Cards in play are: " , cardsInPlay);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+if (cardsInPlay.length === 2) {
+	console.log("Two cards are in play")
+  }
+  
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+  }
+  else {
+  	alert("Sorry,try again!")
+  }
 
-console.log("User flipped " + cardOne + "!");
+console.log("User flipped " + cardsInPlay[0] +" and "+ cardsInPlay[1] + "!");
