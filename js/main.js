5e4f47b76ab074896078694b9bb433f86ca8474e
Anton Melbardis <melbardis@gmail.com>
2017-06-09 13:26:16 +0300

    finished the game code and added code comments

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
+//Creating variable with card properties
 var cards = [
    {
        rank: 'queen',
        suit: 'hearts',
-       cardImage: "images/queen-of-hearts.png"
+       cardImage: 'images/queen-of-hearts.png'
    },
    {
        rank: 'queen',
        suit: 'diamonds',
-       cardImage: "images/queen-of-diamonds.png"
+       cardImage: 'images/queen-of-diamonds.png'
    },
       {
        rank: 'king',
        suit: 'hearts',
-       cardImage: "images/king-of-hearts.png"
+       cardImage: 'images/king-of-hearts.png'
    },
       {
        rank: 'king',
        suit: 'diamonds',
-       cardImage: "images/king-of-diamonds.png"
+       cardImage: 'images/king-of-diamonds.png'
    },
 ];
-
+//Creating an empty array for selected cards
 var cardsInPlay = [];
 
 
-
+//Function to check if user got a match
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -34,19 +35,31 @@ var checkForMatch = function () {
 	}
 };
 
-
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+//Function to place the selected cards to to array of playing cards
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2){
-
 		checkForMatch();
-		
+		cardsInPlay = [];
 	}
-};
 
-flipCard(0);
-flipCard(1);
+
+};
+//Function to create a board and opening them up on user click
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+
+}
+
+createBoard();
